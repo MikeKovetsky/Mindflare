c2c67e19d52e0e346af3a10aa5179777af07c9f2
Alex Romanov <romanovalexandrvitalievich@gmail.com>
2018-07-15 14:11:25 +0300

    Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        new UglifyJSPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        watchContentBase: true
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+        {
+            test: /\.less$/,
+            loader: [{
+                loader: 'style-loader' // creates style nodes from JS strings
+            }, {
+                loader: 'css-loader' // translates CSS into CommonJS
+            }, {
+                loader: 'less-loader' // compiles Less to CSS
+            }]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+            'file-loader'
+            ]
+        }
+        ]
+    },
+};
