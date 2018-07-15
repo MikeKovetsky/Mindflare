c31acf4762e5d6326432d9260793277865002dce
Alex Romanov <romanovalexandrvitalievich@gmail.com>
2018-07-15 15:20:58 +0300

    Webpack configuration fork html in dist

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.less';
 import Icon from './icon.jpg';
+import './index.html';
 
 function component() {
     
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
             }]
         },
         {
-            test: /\.(png|svg|jpg|gif)$/,
+            test: /\.(png|svg|jpg|gif|html)$/,
             use: [
             'file-loader'
             ]
